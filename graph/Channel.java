672d94a2deb62826f66a830f2fbbd53de57340ed
Chirag Manwani <cmkmanwani@gmail.com>
2020-04-14 17:48:10 +0530

    Added implementation for Channel class

--- graph/Channel.java
+++ graph/Channel.java
@@ -1,8 +1,37 @@
 package graph;
 
+import util.ChannelStatus;
+
 public class Channel {
+    private Node n;
+    private int weight;
+    private ChannelStatus status;
 
-    Channel() {
+    public Channel() {
+        n = null;
+        weight = -1;
+        status = ChannelStatus.NULL;
+    }
 
+    public Channel(Node n, int weight) {
+        this.n = n;
+        this.weight = weight;
+        this.status = ChannelStatus.BASIC;
+    }
+
+    public Node getN() {
+        return n;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public ChannelStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ChannelStatus status) {
+        this.status = status;
     }
 }
