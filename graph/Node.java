1394de388250646f1a52eb1912a28316e970cefc
Chirag Manwani <cmkmanwani@gmail.com>
2020-04-14 21:20:51 +0530

    Node now extends Thread, added run function. Added LinkedBlockingQueue to Node. Added implementations for addMessage and processMessage. Added skeleton for GHS functions

--- graph/Node.java
+++ graph/Node.java
@@ -1,10 +1,13 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
 
+import util.MType;
+import util.Message;
 import util.NodeStatus;
 
-public class Node implements Runnable {
+public class Node extends Thread {
     private NodeStatus status;
     private int nodeID;
     private int level;
@@ -17,6 +20,12 @@ public class Node implements Runnable {
     private Channel parent;
 
     private int recP;
+    private LinkedBlockingQueue<Message> q;
+
+
+    /*
+        Node Constructor Functions
+    */
 
     public Node() {
         status = NodeStatus.NULL;
@@ -28,6 +37,7 @@ public class Node implements Runnable {
         bestCh = null;
         parent = null;
         recP = -1;
+        q = null;
     }
 
     public Node(int nodeID) {
@@ -40,17 +50,119 @@ public class Node implements Runnable {
         bestCh = null;
         parent = null;
         recP = 0;
+        q = new LinkedBlockingQueue<>();
     }
 
+    /*
+        Helper Functions
+    */
+
     public void addChannel(Channel c) {
-        if(c != null) {
+        if (c != null) {
             channels.add(c);
         }
     }
 
+    public void addMessage(Message m) {
+        try {
+            q.put(m);
+        } catch (InterruptedException e) {
+            System.out.println("Node interrupted while adding message");
+        }
+    }
+
+    private void processMessage(Message m) {
+        MType mType = m.getType();
+        switch(mType){
+            case WAKEUP:
+                System.out.println("Wakeup Message Received");
+                break;
+            case CONNECT:
+                System.out.println("Connect Message Received");
+                break;
+            case INITITATE:
+                System.out.println("Initiate Message Received");
+                break;
+            case TEST:
+                System.out.println("Test Message Received");
+                break;
+            case ACCEPT:
+                System.out.println("Accept Message Received");
+                break;
+            case REJECT:
+                System.out.println("Reject Message Received");
+                break;
+            case REPORT:
+                System.out.println("Report Message Received");
+                break;
+            case CHANGEROOT:
+                System.out.println("Changeroot Message Received");
+                break;
+            default:
+                System.out.println("Improper message code");
+        }
+
+    }
+
+    /*
+        Thread run function
+    */
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        while (true) {
+            try {
+                Message m = q.take();
+                processMessage(m);
+            } catch (InterruptedException e) {
+                System.out.println();
+            }
+        }
+    }
+
+    /*
+        GHS Functions
+    */
+    public void wakeUp() {
+
+    }
+
+    public void connect(Message m) {
+
+    }
+
+    public void initiate(Message m) {
+
+    }
+
+    public void test(Message m) {
+
+    }
+
+    public void test() {
+
+    }
+
+    public void accept(Message m) {
+
+    }
+
+    public void reject(Message m) {
+
+    }
+
+    public void report(Message m) {
+
+    }
+
+    public void report() {
+
+    }
+
+    public void changeroot(Message m) {
+
+    }
+
+    public void changeroot() {
 
     }
 }
