975434c248e32225e0822e69a16ba103f2249519
Chirag Manwani <cmkmanwani@gmail.com>
2020-04-14 14:38:10 +0530

    Added implementation for Message class

--- util/Message.java
+++ util/Message.java
@@ -1,8 +1,38 @@
 package util;
 
 public class Message {
+    private int type;
+    private int level;
+    private int fID;
+    private int state;
 
-    Message() {
+    public Message() {
+        type = -1;
+        level = -1;
+        fID = -1;
+        state = -1;
+    }
 
+    public Message(int type, int level, int fID, int state) {
+        this.type = type;
+        this.level = level;
+        this.fID = fID;
+        this.state = state;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getfID() {
+        return fID;
+    }
+
+    public int getState() {
+        return state;
     }
 }
