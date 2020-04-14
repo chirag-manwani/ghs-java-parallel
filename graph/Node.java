bc2f62d1bab64b080815d761e9be9448a5016b46
Chirag Manwani <cmkmanwani@gmail.com>
2020-04-14 17:53:30 +0530

    Added Basic Node class functions. GHS functions will be added later. Node implements Runnable

--- graph/Node.java
+++ graph/Node.java
@@ -1,8 +1,56 @@
 package graph;
 
-public class Node {
+import java.util.ArrayList;
 
-    Node() {
+import util.NodeStatus;
+
+public class Node implements Runnable {
+    private NodeStatus status;
+    private int nodeID;
+    private int level;
+    private int bestWeight;
+
+    private ArrayList<Channel> channels;
+
+    private Channel testCh;
+    private Channel bestCh;
+    private Channel parent;
+
+    private int recP;
+
+    public Node() {
+        status = NodeStatus.NULL;
+        nodeID = -1;
+        level = -1;
+        bestWeight = -1;
+        channels = null;
+        testCh = null;
+        bestCh = null;
+        parent = null;
+        recP = -1;
+    }
+
+    public Node(int nodeID) {
+        this.nodeID = nodeID;
+        status = NodeStatus.SLEEP;
+        level = 0;
+        bestWeight = -1;
+        channels = new ArrayList<>();
+        testCh = null;
+        bestCh = null;
+        parent = null;
+        recP = 0;
+    }
+
+    public void addChannel(Channel c) {
+        if(c != null) {
+            channels.add(c);
+        }
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
 
     }
 }
