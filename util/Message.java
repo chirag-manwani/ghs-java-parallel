3c9101a7295b08151dc9f372816947c3461a4466
Chirag Manwani <cmkmanwani@gmail.com>
2020-04-14 17:52:29 +0530

    Changed type of message type.

--- util/Message.java
+++ util/Message.java
@@ -1,26 +1,26 @@
 package util;
 
 public class Message {
-    private int type;
+    private MType type;
     private int level;
     private int fID;
     private int state;
 
     public Message() {
-        type = -1;
+        type = MType.NULL;
         level = -1;
         fID = -1;
         state = -1;
     }
 
-    public Message(int type, int level, int fID, int state) {
+    public Message(MType type, int level, int fID, int state) {
         this.type = type;
         this.level = level;
         this.fID = fID;
         this.state = state;
     }
 
-    public int getType() {
+    public MType getType() {
         return type;
     }
 
